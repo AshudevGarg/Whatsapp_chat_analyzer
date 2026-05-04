c391a161961e0215de2b37844d7a11246a16eb0e
AshudevGarg <ashudevgarg@gmail.com>
2026-05-05 03:39:36 +0530

    Fixed 24-hour WhatsApp export support

--- preprocessor.py
+++ preprocessor.py
@@ -9,11 +9,11 @@ def preprocess(data):
     data = data.replace('\u202f', ' ')
     data = data.replace('\xa0', ' ')
 
-    # Android export pattern
-    android_pattern = r'(\d{1,2}/\d{1,2}/\d{2,4}),\s(\d{1,2}:\d{2}\s?[APMapm]{2})\s-\s'
+    # Android format (supports 24hr + AM/PM)
+    android_pattern = r'(\d{1,2}/\d{1,2}/\d{2,4}),\s(\d{1,2}:\d{2}(?::\d{2})?\s?(?:[APMapm]{2})?)\s-\s'
 
-    # iPhone export pattern
-    iphone_pattern = r'\[(\d{1,2}/\d{1,2}/\d{2,4}),\s(\d{1,2}:\d{2}:\d{2}\s?[APMapm]{2})\]'
+    # iPhone format
+    iphone_pattern = r'\[(\d{1,2}/\d{1,2}/\d{2,4}),\s(\d{1,2}:\d{2}:\d{2}\s?(?:[APMapm]{2})?)\]'
 
     # Detect format
     if re.search(iphone_pattern, data):
